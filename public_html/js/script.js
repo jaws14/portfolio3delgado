505e9f0857a011d4d2061cf952af9b0797e04909
pablo j delgado <fluffydemonkey@gmail>
2015-01-21 15:04:41 -0800

    added code

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,4 +1,23 @@
-$("document").ready(function(){
-      alert( "welcome" );
-});
- 
+$('document').ready(function(){
+$('.yeah').css('background-color','yellow');
+
+$('#superhumans').accordion ({header: "h3"});
+
+$('.2').css({)'visibility': 'hidden', 'position':'absolute'});
+
+$('div:last p:nth-child(3)').css ('background-color', 'pink');
+
+$('div:last p:nth-child(3)').css('background-color', 'pink');
+
+$("h1").bind('mouseover', mouseoverme).bind('mouseout', mouseoutme);
+
+$('#replaceWtext').bind('click', replaceWtext);
+
+$(#randpara).bind('click', addApara);
+
+$('#removepara').bind('click', removeAPara);
+
+$('#show').css('visibility', 'hidden');
+
+$('#hide').bind('click', hidethepage);
+
