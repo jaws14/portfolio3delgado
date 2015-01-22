222987949de4fad3196fab2ba6bf56d7334d18f1
pablo j delgado <fluffydemonkey@gmail>
2015-01-22 15:03:18 -0800

    added code

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,24 +1,24 @@
-$('document').ready(function(){
-$('.yeah').css('background-color','yellow');
+$('document').ready(function() {
+    $('.yeah').css('background-color', 'yellow');
 
-$('#superhumans').accordion ({header: "h3"});
+    $('#superhumans').accordion({header: "h3"});
 
-$('.2').css({'visibility': 'hidden', 'position':'absolute'});
+    $('.2').css({'visibility': 'hidden', 'position': 'absolute'});
 
-$('div:last p:nth-child(3)').css ('background-color', 'pink');
+    $('div:last p:nth-child(3)').css('background-color', 'pink');
 
-$('div:last p:nth-child(3)').css('background-color', 'pink');
+    $('div:last p:nth-child(3)').css('background-color', 'pink');
 
-$("h1").bind('mouseover', mouseoverme).bind('mouseout', mouseoutme);
+    $("h1").bind('mouseover', mouseoverme).bind('mouseout', mouseoutme);
 
-$('#replaceWtext').bind('click', replaceWtext);
+    $('#replaceWtext').bind('click', replaceWtext);
 
-$(".randpara").bind('click', addApara);
+    $(".randpara").bind('click', addApara);
 
-$('#removepara').bind('click', removeAPara);
+    $('#removepara').bind('click', removeAPara);
 
-$('#show').css('visibility', 'hidden');
+    $('#show').css('visibility', 'hidden');
 
-$('#hide').bind('click', hidethepage);
+    $('#hide').bind('click', hidethepage);
 
 });
